eff99cf1c369e212e50a5be5001e88793323561c
ilSyAbRi <iliasssyabri1@gmail.com>
2026-02-04 19:20:50 +0100

    do it but whit rec

---
+++ python_00/ex5/ft_count_harvest_recursive.py
@@ -0,0 +1,8 @@
+def ft_count_harvest_recursive():
+    n = int(input("Days until harvest ")):
+        ft_rec(day):
+            if (day > n):
+                return
+            else:
+                print("day ",day):
+    ft_rec(1)
